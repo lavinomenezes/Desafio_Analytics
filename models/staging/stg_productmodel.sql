5e637147b92c0704b29032eee3838df9369e544f
lavino pereira de menezes neto <lavino.pereira@indicium.tech>
2023-07-03 19:03:39 -0300

    remove staging test

--- models/staging/stg_productmodel.sql
+++ models/staging/stg_productmodel.sql
@@ -2,7 +2,8 @@ with productmodel as (
     select
         productmodelid,
         name as product_model_name,
-    from {{ source('dev_lavino','productmodel') }}     
+    from {{ source('dev_lavino','productmodel') }}    
+    order by productmodelid 
 )
 select *
 from productmodel
